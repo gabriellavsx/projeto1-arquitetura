348e05d2f0267353ca40f8478919239971bb0092
Maria Gabriella Victor <109552474+gabriellavsx@users.noreply.github.com>
2024-07-29 08:16:41 -0300

    padroes

--- 01-design-patterns-template-strategy/index.js
+++ 01-design-patterns-template-strategy/index.js
@@ -1,7 +1,86 @@
 import CidadesHTMLReport from './src/CidadesHTMLReporter.js';
 import CidadesTXTReport from './src/CidadesTXTReporter.js';
 
+
+// Factory Method
+class ReporterFactory {
+  static createReporter(type) {
+    if (type === 'html') {
+      return new CidadesHTMLReportAdapter();
+    } else if (type === 'txt') {
+      return new CidadesTXTReportAdapter();
+    } else {
+      throw new Error('Formato não suportado');
+    }
+  }
+}
+
+// Adapters
+class CidadesHTMLReportAdapter {
+  constructor() {
+    this.reporter = new CidadesHTMLReport();
+  }
+
+  read(caminho) {
+    this.reporter.ler(caminho);
+  }
+
+  parse() {
+    this.reporter.parse();
+  }
+
+  generate() {
+    return this.reporter.reportar();
+  }
+}
+
+class CidadesTXTReportAdapter {
+  constructor() {
+    this.reporter = new CidadesTXTReport();
+  }
+
+  read(caminho) {
+    this.reporter.ler(caminho);
+  }
+
+  parse() {
+    this.reporter.parse();
+  }
+
+  generate() {
+    return this.reporter.reportar();
+  }
+}
+
+// Strategy
+class ReportGenerator {
+  constructor(strategy) {
+    this.strategy = strategy;
+  }
+
+  generateReport(caminho) {
+    this.strategy.read(caminho);
+    this.strategy.parse();
+    return this.strategy.generate();
+  }
+}
+
+// Código principal
 const [cmd, filename, format] = process.argv;
+
+try {
+  const reporter = ReporterFactory.createReporter(format);
+  const reportGenerator = new ReportGenerator(reporter);
+  const report = reportGenerator.generateReport('./data/cidades-2.json');
+  console.log(report);
+} catch (error) {
+  console.error(error.message);
+}
+
+
+
+
+/*const [cmd, filename, format] = process.argv;
 
 
 if (format === 'html') {
@@ -17,4 +96,4 @@ if (format === 'txt') {
   reporter.parse();
   let html = reporter.reportar();
   console.log(html);
-}
+}*/
